4f5de6abea25f140bdea2f89ff160c86485670ef
ankitasingh8762-maker <ankitasingh8762@gmail.com>
2026-09-24 15:48:42 +0530

    lab13

---
+++ lab13/ques1.i.sh
@@ -0,0 +1,6 @@
+artificial intelligence, and more.
+It was created by Guido van Rossum in 1991 and
+Readable and simple syntax - Python code often looks like plain English,
+Cross-platform - Works on Windows, macOS, Linux, and many other systems.
+Extensive libraries - Python has built-in modules and
+data analysis, networking, and visualization.
